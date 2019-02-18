b5477f81829088ebd6752c080943326862d0e17a
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-18 11:40:33 -0800

    dynamically created game board and populated cells with buttons assigned values from array of board game objects

--- src/index/index.js
+++ src/index/index.js
@@ -1,16 +1,16 @@
 const formNode = document.getElementById('user-input');
 
-
-
 formNode.addEventListener('submit', function(event){
     event.preventDefault();
     const formData = new FormData(formNode);
-
-
 
     const userInfo = {
         playerCake: formData.get('player-cake'),
         playerDeath: formData.get('player-death')    
     };
-    console.log(userInfo);
-})
+
+    const json = JSON.stringify(userInfo);
+    window.localStorage.setItem('userInfo', json);
+
+    window.location = './game.html';
+});
