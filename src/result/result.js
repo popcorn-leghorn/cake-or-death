c2965c7b3c92a9473bc8730a5875386e889a79dc
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-21 16:57:44 -0800

    testing something

--- src/result/result.js
+++ src/result/result.js
@@ -16,7 +16,7 @@ switch(resultToFind) {
     case 'cake':
         endMessageNode.textContent = userInfo.playerCake;
         endMessageNode.innerHTML += ' wins!<br> Let them eat CAKE!';
-        endImageNode.src = '../../assets/cake-1.png';
+        endImageNode.src = './assets/cake-1.png';
         endImageNode.id = 'cake-wins';
         endImageNode.alt = 'a wedding cake';
         break;
@@ -24,7 +24,7 @@ switch(resultToFind) {
     case 'death':
         endMessageNode.textContent = userInfo.playerDeath;
         endMessageNode.innerHTML += ' wins! 1000 years of DEATH falls upon the land...';
-        endImageNode.src = '../../assets/grim-victory.png';
+        endImageNode.src = './assets/grim-victory.png';
         endImageNode.id = 'death-wins';
         endImageNode.alt = 'the grim reaper';
         break;
@@ -33,7 +33,7 @@ switch(resultToFind) {
         playTie();
         endMessageNode.textContent = userInfo.playerCake + ' and ' + userInfo.playerDeath;
         endMessageNode.innerHTML += ' were not strong enough to defeat one another, an unfortunate tie has resulted from their inadequacies.';
-        endImageNode.src = '../../assets/cake-or-death.png';
+        endImageNode.src = './assets/cake-or-death.png';
         endImageNode.id = 'tie';
         endImageNode.alt = 'a cake and a skull that reads cake or death';
         break;
